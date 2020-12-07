548399722c8eb2f825055add9461c80b45a3a4fe
Daniel Treviño <danielivert@gmail.com>
2020-12-07 07:57:58 +0100

    feat: update macos dock apps

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,13 +2,11 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Brave Browser.app"
-dockutil --no-restart --add "/System/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/System/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/Slack.app"
-dockutil --no-restart --add "/Applications/iTerm.app"
 
 killall Dock
