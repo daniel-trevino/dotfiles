d289a50b418c04cfd2baf17a3e0daec8cc2605ca
Daniel Treviño <danielivert@gmail.com>
2021-10-18 14:08:41 +0200

    feat: add new software

--- runcom/.oh-my-zsh/custom/aliases.zsh
+++ runcom/.oh-my-zsh/custom/aliases.zsh
@@ -27,6 +27,7 @@ alias yr="yarn remove"
 alias yri="rm -r node_modules && yarn"
 alias yc="yarn clean"
 alias yu="yarn upgrade --latest"
+alias yui="yarn upgrade-interactive --latest" # Upgrades on yarn workspaces
 alias yb="yarn build"
 alias yt="yarn test"
 
